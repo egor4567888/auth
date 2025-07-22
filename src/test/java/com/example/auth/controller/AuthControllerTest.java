eae9a516face6ba631e70ea112aa11badb2e41e5
egor4567888 <egor456788@gmail.com>
2025-07-22 17:27:24 +0300

    fixed 403 in AuthControllerTest

--- src/test/java/com/example/auth/controller/AuthControllerTest.java
+++ src/test/java/com/example/auth/controller/AuthControllerTest.java
@@ -1,11 +1,13 @@
 package com.example.auth.controller;
 
+import com.example.auth.config.SecurityConfig;
 import com.example.auth.dto.JwtAuthDto;
 import com.example.auth.dto.RefreshTokenDto;
 import com.example.auth.dto.UserCredentialsDto;
 import com.example.auth.dto.UserDto;
 import com.example.auth.jwt.JwtService;
 import com.example.auth.model.MyUser;
+import com.example.auth.repository.UserRepository;
 import com.example.auth.service.AuthService;
 import com.example.auth.service.MyUserDetailsService;
 import org.junit.jupiter.api.Test;
@@ -13,8 +15,10 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -22,11 +26,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@Import(SecurityConfig.class)
 @WebMvcTest(AuthController.class)
 public class AuthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository userRepository;
 
     @MockBean
     private AuthService authService;
@@ -65,7 +73,7 @@ public class AuthControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(creds)))
                 .andExpect(status().is5xxServerError())
-                .andExpect(content().string(org.hamcrest.Matchers.containsString("Authentication failed")));
+                .andExpect(content().string(org.hamcrest.Matchers.containsString("Internal error Bad credentials")));
     }
 
     @Test
