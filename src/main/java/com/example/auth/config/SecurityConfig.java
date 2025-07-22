849ff4f7795610b6604bf35eecf25a729452b329
egor4567888 <egor456788@gmail.com>
2025-07-22 11:03:54 +0300

    roles for endpoints

--- src/main/java/com/example/auth/config/SecurityConfig.java
+++ src/main/java/com/example/auth/config/SecurityConfig.java
@@ -2,26 +2,39 @@ package com.example.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder){
-        UserDetails admin = User.builder().username("admin").password(encoder.encode("admin")).build();
-        UserDetails prem = User.builder().username("prem").password(encoder.encode("prem")).build();
-        UserDetails guest = User.builder().username("guest").password(encoder.encode("guest")).build();
+        UserDetails admin = User.builder().username("admin").password(encoder.encode("admin")).roles("ADMIN").build();
+        UserDetails prem = User.builder().username("prem").password(encoder.encode("prem")).roles("PREMIUM_USER").build();
+        UserDetails guest = User.builder().username("guest").password(encoder.encode("guest")).roles("GUEST").build();
 
         return new InMemoryUserDetailsManager(admin,prem,guest);
-
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.requestMatchers("/welcome").permitAll()
+                .requestMatchers("api/**").authenticated())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll).build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
--- src/main/java/com/example/auth/controller/TestController.java
+++ src/main/java/com/example/auth/controller/TestController.java
@@ -1,21 +1,29 @@
 package com.example.auth.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/guest")
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome";
+    }
+
+    @GetMapping("/api/guest")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')|| hasAuthority('ROLE_PREMIUM_USER')|| hasAuthority('ROLE_GUEST')")
     public String guest(){
         return "guest";
     }
 
-    @GetMapping("/prem")
-    public String prem(){
-        return "prem";
+    @GetMapping("/api/premium_user")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')|| hasAuthority('ROLE_PREMIUM_USER')")
+    public String premium_user(){
+        return "premium_user";
     }
-
-    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @GetMapping("/api/admin")
     public String admin(){
         return "admin";
     }
